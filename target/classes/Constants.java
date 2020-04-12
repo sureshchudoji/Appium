fbec2e1fdc8d016403e07ffaf24e27226a7ba133
sureshchudoji <suresh.chudoji@gmail.com>
2020-04-12 13:34:58 +0530

    Code refactoring

---
+++ target/classes/Constants.java
@@ -0,0 +1,40 @@
+package com.ebay.resources;
+
+public class Constants {
+
+	public Constants() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static String DEVICE_NAME = "deviceName";
+	public static String REDMI_K20 = "Redmi-K20";
+	public static String UDID = "udid";
+	public static String DEVICE_ID = "23f2db35";
+	public static String AUTOMATION_NAME = "automationName";
+	public static String UI_AUTOMATOR1 = "Uiautomator1";
+	public static String PLATFORM_NAME = "platformName";
+	public static String ANDROID = "Android";	
+	public static String PLATFORM_VERSION = "platformVersion";
+	public static double ANDROID_VERSION = 10.0;
+	public static String WAIT_FOR_IDLE_TIMEOUT = "waitForIdleTimeout";
+	public static int IDLE_TIMEOUT = 3000;
+	public static String DISABLE_WINDOW_ANIMATION = "disableWindowAnimation";	
+	public static String APP_PACKAGE_ = "appPackage";
+	public static String APP_PACKAGE_NAME_ = "com.amazon.mShop.android.shopping";	
+	public static String APP_ACTIVITY = "appActivity";
+	public static String APP_ACTIVITY_NAME = "com.amazon.mShop.home.HomeActivity";
+	public static String NO_RESET = "noReset";
+	public static boolean TRUE  =  true;
+	public static boolean FALSE  =  false;
+	
+	//cap.setCapability("deviceName", "Redmi-K20");
+	//cap.setCapability("udid", "23f2db35");
+	//cap.setCapability("automationName", "Uiautomator1");
+	//cap.setCapability("platformName", "Android");
+	//cap.setCapability("platformVersion", "10.0");
+	//cap.setCapability("waitForIdleTimeout", "3000");
+	//cap.setCapability("disableWindowAnimation", "true");
+	//cap.setCapability("appPackage", "com.amazon.mShop.android.shopping");
+	//cap.setCapability("appActivity", "com.amazon.mShop.home.HomeActivity");
+	//cap.setCapability("noReset", "false");
+}
